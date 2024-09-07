3c63fd77335daedeec28a69dac2aac3fb5359ea8
wynnzuo <wynnzuo@163.com>
2024-09-08 02:17:56 +0800

    init

---
+++ src/sort/SelectionSort.java
@@ -0,0 +1,17 @@
+package sort;
+
+import java.util.Arrays;
+
+//选择排序
+//时间复杂度O(N²)
+public class SelectionSort {
+    public static int[] selection(int[] array) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[] { 5, 4, 3, 2, 1 };
+        int[] outArray = selection(array);
+        System.out.println(Arrays.toString(outArray));
+    }
+}
